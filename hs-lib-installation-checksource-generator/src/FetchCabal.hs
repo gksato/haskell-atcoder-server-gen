964c0d0083f8f7671c02cafe760f34e7931c4779
Genki Sato <13384803+gksato@users.noreply.github.com>
2023-03-11 09:24:34 +0900

    Integrate checksource generator

---
+++ hs-lib-installation-checksource-generator/src/FetchCabal.hs
@@ -0,0 +1,136 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module FetchCabal where
+
+import Network.HTTP.Types.Header ( hAccept )
+import Network.HTTP.Client.TLS
+import Network.HTTP.Client
+import Data.ByteString ( ByteString )
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import Control.Monad.Catch ( Exception, MonadThrow(..) )
+import Distribution.Types.GenericPackageDescription (GenericPackageDescription)
+import Distribution.PackageDescription.Parsec (parseGenericPackageDescription, runParseResult)
+import Distribution.Parsec.Warning (PWarning)
+import qualified Distribution.Types.Version as CVer
+import Data.List.NonEmpty (NonEmpty)
+import Distribution.Parsec.Error (PError)
+import Distribution.System (Platform (Platform), Arch (X86_64), OS (Linux))
+import Distribution.Compiler (CompilerId (CompilerId), CompilerFlavor (GHC), unknownCompilerInfo, AbiTag (NoAbiTag), CompilerInfo)
+import Distribution.Types.ComponentRequestedSpec (ComponentRequestedSpec (OneComponentRequestedSpec))
+import Distribution.PackageDescription (ComponentName(CLibName), defaultLibName, Dependency, FlagAssignment, PackageDescription,  mkFlagAssignment, mkFlagName)
+import Distribution.PackageDescription.Configuration (finalizePD)
+import Data.Bifunctor (Bifunctor(first))
+import Data.Either (fromRight)
+import Data.Map (Map)
+import qualified Data.Map.Strict as Map
+import Data.Aeson.TH (Options(..), deriveJSON, defaultOptions)
+import Data.Char (toLower)
+import Data.Aeson (decode)
+import qualified Data.Version as BVer
+
+httpBSMgrIO :: Manager -> Request -> IO (Response ByteString)
+httpBSMgrIO req mgr = withResponse mgr req $ \ resp -> do
+  bss <- brConsume $ responseBody resp
+  return resp { responseBody = BS.concat bss }
+
+type PackageString = String
+type VersionString = String
+
+hackageCabalURI :: PackageString -> Maybe VersionString -> String
+hackageCabalURI pkg ver
+  = "https://hackage.haskell.org/package/" ++ pkg
+  ++ maybe [] ('-':) ver ++ "/" ++ pkg ++ ".cabal"
+
+hackageCabalRequest
+  :: MonadThrow m => PackageString -> Maybe VersionString -> m Request
+hackageCabalRequest pkg ver = parseRequest $ "GET " ++ uri
+  where
+    uri = hackageCabalURI pkg ver
+
+data CabalParseError
+  = CabalParseError (Maybe CVer.Version) (NonEmpty PError) [PWarning]
+  deriving Show
+
+instance Exception CabalParseError
+
+askHackageForGPD
+  :: Manager
+  -> PackageString
+  -> Maybe VersionString
+  -> IO (GenericPackageDescription, [PWarning])
+askHackageForGPD mgr pkg ver = do
+  req <- hackageCabalRequest pkg ver
+  resp <- httpBSMgrIO mgr req { checkResponse = throwErrorStatusCodes }
+  let (warns, parseRes) = runParseResult
+        $ parseGenericPackageDescription
+        $ responseBody resp
+  case parseRes of
+    Left (mver, errors) -> throwM $ CabalParseError mver errors warns
+    Right res -> return (res, warns)
+
+platformLinuxX64 :: Platform
+platformLinuxX64 = Platform X86_64 Linux
+
+ghc944 :: CompilerId
+ghc944 = CompilerId GHC (CVer.mkVersion [9,4,4])
+
+compilerInfoGHC944 :: CompilerInfo
+compilerInfoGHC944 = unknownCompilerInfo ghc944 NoAbiTag
+
+mainLibSpec :: ComponentRequestedSpec
+mainLibSpec = OneComponentRequestedSpec $ CLibName defaultLibName
+
+simpleFinalizePD :: FlagAssignment -> GenericPackageDescription
+  -> Either [Dependency] (PackageDescription, FlagAssignment)
+simpleFinalizePD fa
+  = finalizePD fa mainLibSpec (const True) platformLinuxX64
+    compilerInfoGHC944 []
+
+type FlagNameString = String
+
+askHackageForPD
+  :: Manager
+  -> PackageString
+  -> Maybe VersionString
+  -> [(FlagNameString, Bool)]
+  -> IO (PackageDescription, FlagAssignment, [PWarning])
+askHackageForPD mgr pkg ver flgs = do
+  (gpd, warns) <- askHackageForGPD mgr pkg ver
+  let fa = mkFlagAssignment $ map (first mkFlagName) flgs
+  let (res, fa') = fromRight (error "Impossible")
+        $ simpleFinalizePD fa gpd
+  return (res, fa', warns)
+
+data PackagePreferrence
+  = PPNormal | PPUnpreferred | PPDeprecated
+  deriving (Show)
+$(deriveJSON
+  defaultOptions { constructorTagModifier = map toLower . drop 2 }
+ ''PackagePreferrence)
+  
+hackagePackageURI :: PackageString -> String
+hackagePackageURI pkg = "https://hackage.haskell.org/package/" ++ pkg
+
+hackagePkgVersionsRequest
+  :: MonadThrow m => PackageString -> m Request
+hackagePkgVersionsRequest pkg
+  = (\req -> req { requestHeaders = [(hAccept, "application/json")] })
+    <$> parseRequest ("GET " ++ hackagePackageURI pkg)
+
+data JSONError = JSONError
+  deriving (Show)
+
+instance Exception JSONError
+
+askHackageForPkgVersions
+  :: Manager
+  -> PackageString
+  -> IO (Map BVer.Version PackagePreferrence)
+askHackageForPkgVersions mgr pkg = do
+  req <- hackagePkgVersionsRequest pkg
+  resp <- httpLbs req { checkResponse = throwErrorStatusCodes } mgr
+  let res = responseBody resp
+  case decode res of
+    Nothing -> throwM JSONError
+    Just a -> return a
---
+++ hs-lib-installation-checksource-generator/src/GenSource.hs
@@ -0,0 +1,56 @@
+module GenSource where
+import Distribution.Types.PackageDescription (PackageDescription(package, library, licenseFiles), license)
+import Distribution.Types.Library (Library(exposedModules))
+import qualified Data.Text.Lazy as TLazy
+import qualified Data.Text.Lazy.IO as TLazy
+import qualified Data.Text.Lazy.Builder as TLazyB
+import Distribution.Pretty ( prettyShow )
+import Data.List (intersperse)
+import Distribution.Types.PackageId (PackageIdentifier(..))
+
+genSourceWithExposedImports
+  :: [PackageDescription]
+  -> TLazy.Text
+genSourceWithExposedImports packages = TLazyB.toLazyText
+  $ moduleDecl
+  <> foldr
+     (\descr rest ->
+         "-- " <> TLazyB.fromString (prettyShow $ package descr)
+         <> newLine
+         <> foldr
+            (\modl rest' ->
+               importQualified
+               <> TLazyB.fromString (prettyShow modl)
+               <> newLine
+               <> rest')
+            (newLine <> rest)
+            (maybe [] exposedModules $ library descr)
+     )
+     mainFct packages
+  where
+    moduleDecl = "module Main where\n\n\n"
+    mainFct = "\nmain :: IO ()\nmain = return ()\n"
+    importQualified = "import qualified "
+    newLine = TLazyB.singleton '\n'
+
+genLicenses
+  :: [PackageDescription]
+  -> TLazy.Text
+genLicenses packages = TLazyB.toLazyText
+  $ foldr
+  (\descr rest ->
+     let urlprefix = TLazyB.fromString
+           "https://hackage.haskell.org/package/"
+           <> TLazyB.fromString (prettyShow $ pkgName $ package descr)
+           <> "/src/" in
+     TLazyB.fromString (prettyShow $ pkgName $ package descr)
+     <> TLazyB.singleton '('
+     <> TLazyB.fromString (prettyShow $ license descr)
+     <> TLazyB.fromString "): "
+     <> foldr (\ path rest -> urlprefix <> TLazyB.fromString (prettyShow path) <> TLazyB.singleton ',' <> rest)
+        (newLine <> rest)
+        (licenseFiles descr))
+  mempty
+  packages
+  where
+    newLine = TLazyB.singleton '\n'
