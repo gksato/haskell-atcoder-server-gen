ff600293d5e8ecf6e325ac873e6b3c928d38dd61
toyboot4e <toyboot4e@gmail.com>
2025-01-15 02:21:49 +0900

    fix source gen to find the `submission` project

--- checksource-gen/app/Main.hs
+++ checksource-gen/app/Main.hs
@@ -5,10 +5,13 @@ module Main where
 
 import Cabal.Plan
 import qualified Data.Map.Strict as Map
+import Data.Maybe (fromJust)
 import qualified Data.Set as Set
 import qualified Network.HTTP.Client.TLS as TLS
 
 import FetchCabal.WithPlan
+import qualified Data.List as L
+import qualified Data.Text as T
 import qualified Data.Text.Lazy.IO as Text
 import GenSource (genSourceWithExposedImports)
 import qualified Cabal.Plan as Plan
@@ -17,7 +20,11 @@ main :: IO ()
 main = do
   pj@PlanJson {..} <- findAndDecodePlanJson
         $ ProjectRelativeToDir "/home/runner/submission"
-  let rootUnit = (pjUnits Map.!) $ head $ Set.toList $ planJsonIdRoots pj
+  let rootUnit = (pjUnits Map.!)
+        . fromJust
+        . L.find (\(Plan.UnitId text) -> T.isPrefixOf (T.pack "submission") text)
+        . Set.toList
+        $ planJsonIdRoots pj
   let deps = Set.toList
         $ ciLibDeps $ uComps rootUnit Map.! CompNameExe "main"
   mgr <- TLS.newTlsManager
@@ -25,7 +32,7 @@ main = do
     $ filter (\ Plan.Unit { .. } -> case uPId of
                  Plan.PkgId (Plan.PkgName pkgname) _
                    -> pkgname /= "ghc-boot-th")
-    $ map (pjUnits Map.!) deps
+    $ map (\dep -> fromJust (Map.lookup dep pjUnits)) deps
   Text.putStr
     $ genSourceWithExposedImports pds
     [("ghc-boot-th-9.8.4", ["GHC.LanguageExtensions.Type",
