0015861a5a38b7a70397aba897a47475165e6db7
Genki Sato <13384803+gksato@users.noreply.github.com>
2025-08-26 20:03:57 +0900

    Update checksource-gen
    Implement configurable workarounds for the cases
    where cabal-plan + Hackage fetch does not work well.

--- checksource-gen/CHANGELOG.md
+++ checksource-gen/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Revision history for hs-lib-installation-checksource-generator
 
+## 0.2.0.0 
+
+* Make the implementation more generic.
+
 ## 0.1.0.0 -- YYYY-mm-dd
 
 * First version. Released on an unsuspecting world.
--- checksource-gen/app/Main.hs
+++ checksource-gen/app/Main.hs
@@ -3,39 +3,16 @@
 
 module Main where
 
-import Cabal.Plan
+import App (run)
+import Config (Workaround(..), defaultConfig, Config(workarounds))
 import qualified Data.Map.Strict as Map
-import Data.Maybe (fromJust)
-import qualified Data.Set as Set
-import qualified Network.HTTP.Client.TLS as TLS
-
-import FetchCabal.WithPlan
-import qualified Data.List as L
-import qualified Data.Text as T
-import qualified Data.Text.Lazy.IO as Text
-import GenSource (genSourceWithExposedImports)
-import qualified Cabal.Plan as Plan
 
 main :: IO ()
-main = do
-  pj@PlanJson {..} <- findAndDecodePlanJson
-        $ ProjectRelativeToDir "/home/runner/submission"
-  let rootUnit = (pjUnits Map.!)
-        . fromJust
-        . L.find (\(Plan.UnitId text) -> T.isPrefixOf (T.pack "submission") text)
-        . Set.toList
-        $ planJsonIdRoots pj
-  let deps = Set.toList
-        $ ciLibDeps $ uComps rootUnit Map.! CompNameExe "main"
-  mgr <- TLS.newTlsManager
-  pds <- mapM (askHackageForUnitPD' mgr pj)
-    $ filter (\ Plan.Unit { .. } -> case uPId of
-                 Plan.PkgId (Plan.PkgName pkgname) _
-                   -> pkgname /= "ghc-boot-th")
-    $ map (pjUnits Map.!) deps
-  Text.putStr
-    $ genSourceWithExposedImports pds
-    [("ghc-boot-th-9.8.4", ["GHC.LanguageExtensions.Type",
-            "GHC.ForeignSrcLang.Type",
-            "GHC.Lexeme"])]
-  return ()
+main = run config
+
+config :: Config
+config = defaultConfig { workarounds = Map.fromList [
+      ("ghc-boot-th", FetchGHCPkg),
+      ("base", FetchGHCPkg),
+      ("text", AddFlagSettings [("simdutf", False)])
+  ]}
